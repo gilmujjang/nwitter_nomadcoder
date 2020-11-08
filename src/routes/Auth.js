19d5e89754637b4d43b294b277228f0c9f22e2c5
gilmujjang <alsanrlf@naver.com>
2020-11-08 14:51:33 +0900

    Authentication

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
+import { firebaseInstance } from '../fbase';
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true)
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {target: {name, value}} = event;
     if(name === "email"){
@@ -11,15 +15,39 @@ const Auth = () => {
       setPassword(value)
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async(event) => {
     event.preventDefault();
+    try {
+      let data;
+      if(newAccount){
+        data = await authService.createUserWithEmailAndPassword(email, password)
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password)
+      }
+      console.log(data);
+    } catch(error){
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async(event) => {
+    const {target:{name},
+    } = event;
+    let provider;
+    if(name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if(name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
   return(
     <div>
       <form onSubmit={onSubmit}>
         <input 
           name = "email" 
-          type="text" 
+          type="email" 
           placeholder="Email" 
           required 
           value={email}
@@ -33,11 +61,13 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log in"></input>
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"}></input>
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
       </div>
     </div>
   )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 
-const Home = () => <span>Home</span>;
-export default Home;
+export default () => {
+  return <span>Home</span>
+}
