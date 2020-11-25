3cf9d4c30b3d43a320aeb754ca6d717d6fe81193
gilmujjang <alsanrlf@naver.com>
2020-11-25 13:50:59 +0900

    realtime nweet

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import React from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from 'routes/Auth';
 import Home from 'routes/Home';
 import Navigation from "components/Navigation";
@@ -7,7 +7,7 @@ import Profile from "routes/Profile";
 
 
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return(
     <Router>
       {isLoggedIn && <Navigation/>}
@@ -15,7 +15,7 @@ const AppRouter = ({ isLoggedIn }) => {
         {isLoggedIn ?(
         <>
           <Route exact path = "/">
-            <Home />
+            <Home userObj={userObj} />
           </Route>
           <Route exact path = "/profile">
             <Profile />
