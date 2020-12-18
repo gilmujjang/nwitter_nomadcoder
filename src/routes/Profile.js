6a5a4e841c68631ee9ccc1fadcf9e21e58053e7d
gilmujjang <alsanrlf@naver.com>
2020-12-19 00:51:09 +0900

    stylecss

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -43,18 +43,28 @@ export default ({userObj,refreshUser}) => {
   };
 
   return (
-    <>
-    <form onSubmit={onSubmit}>
-      <input 
-        type="text" 
-        placeholder="Display name"
+  <div className="container">
+    <form onSubmit={onSubmit} className="profileForm">
+      <input
         onChange={onChange}
+        type="text"
+        autoFocus
+        placeholder="Display name"
         value={newDisplayName}
+        className="formInput"
       />
-      <input 
-        type="submit" placeholder="Update profile"/>
+      <input
+        type="submit"
+        value="Update Profile"
+        className="formBtn"
+        style={{
+          marginTop: 10,
+        }}
+      />
     </form>
-      <button onClick={onLogOutClick}>Log Out</button>
-    </>
-  )
-}
+    <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+      Log Out
+    </span>
+  </div>
+);
+};
